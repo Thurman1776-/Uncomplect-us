9e5e316147246a968ab6c25f743f3691239dfb83
Daniel GARCÍA <thurman.dev1776@gmail.com>
2019-10-27 15:34:53 +0100

    Add ReSwift via Accio

--- Mac-App/Package.swift
+++ Mac-App/Package.swift
@@ -5,15 +5,13 @@ let package = Package(
     name: "Mac-App",
     products: [],
     dependencies: [
-        // add your dependencies here, for example:
-        // .package(url: "https://github.com/User/Project.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/ReSwift/ReSwift.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         .target(
             name: "Mac-App",
             dependencies: [
-                // add your dependencies scheme names here, for example:
-                // "Project",
+                "ReSwift",
             ],
             path: "Mac-App"
         ),
