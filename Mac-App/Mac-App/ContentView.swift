7c96519c99eacfc041af36358827d9b1027ab535
Daniel GARCÍA <thurman.dev1776@gmail.com>
2019-10-27 15:02:08 +0100

    Add Mac project to workspace

---
+++ Mac-App/Mac-App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Mac-App
+//
+//  Created by Daniel GARCÍA on 27.10.19.
+//  Copyright © 2019 Acphut Werkstatt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
