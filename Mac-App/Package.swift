2897ecef42f84df38ee8a9268585c1caf890f773
Daniel GARCÍA <thurman.dev1776@gmail.com>
2019-10-27 15:48:56 +0100

    Linke Nimble via Accio

--- Mac-App/Package.swift
+++ Mac-App/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ReSwift/ReSwift.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
     ],
     targets: [
         .target(
@@ -14,6 +15,7 @@ let package = Package(
             dependencies: [
                 "ReSwift",
                 "Quick",
+                "Nimble"
             ],
             path: "Mac-App"
         ),
