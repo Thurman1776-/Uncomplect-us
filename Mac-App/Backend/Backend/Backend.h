4d2a00b4a356fcd46f89097392b3b52e846bc973
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-01-25 17:05:45 +0100

    Add Backend Xcode proj to workspace

---
+++ Mac-App/Backend/Backend/Backend.h
@@ -0,0 +1,19 @@
+//
+//  Backend.h
+//  Backend
+//
+//  Created by Daniel GARCÍA on 25.01.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Backend.
+FOUNDATION_EXPORT double BackendVersionNumber;
+
+//! Project version string for Backend.
+FOUNDATION_EXPORT const unsigned char BackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Backend/PublicHeader.h>
+
+
