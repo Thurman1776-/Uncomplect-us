9fd1cf69de7d5a6b781d3c47a417a21ec91a5f33
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-01 17:52:55 +0100

    Add Yaml parser dependency

--- Mac-App/Backend/Package.swift
+++ Mac-App/Backend/Package.swift
@@ -8,12 +8,14 @@ let package = Package(
         .package(url: "https://github.com/ReSwift/ReSwift.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "Backend",
             dependencies: [
                 "ReSwift",
+                "Yams",
             ],
             path: "Backend"
         ),
