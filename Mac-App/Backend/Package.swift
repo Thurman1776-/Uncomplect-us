e0fe66788ec551fb0ab4e8cb0d9cbb604539d4ec
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-02 13:30:28 +0100

    Link Yams to testing target

--- Mac-App/Backend/Package.swift
+++ Mac-App/Backend/Package.swift
@@ -24,6 +24,7 @@ let package = Package(
             dependencies: [
                 "Quick",
                 "Nimble",
+                "Yams",
             ],
             path: "BackendTests"
         ),
