3aeb37272b9188e92db6cc773c413c1c72eb26a9
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-15 19:19:49 +0100

    Fine grain swiftdeps parsing to omit certain types

--- Mac-App/Backend/Backend/Parsers/SwiftDepsParser/SwiftDepsParser.swift
+++ Mac-App/Backend/Backend/Parsers/SwiftDepsParser/SwiftDepsParser.swift
@@ -1,3 +1,5 @@
+/// Parsing omits private declarations, ocurrances of `Self`, system symbols (see `systemSymbols` declarion)
+/// and objects with `NS` prefix
 
 func parseSwiftDeps(_ swiftDeps: [SwiftDeps]) -> [DependencyTree] {
     var result = [DependencyTree]()
@@ -6,8 +8,11 @@ func parseSwiftDeps(_ swiftDeps: [SwiftDeps]) -> [DependencyTree] {
         dep.providesTopLevel.forEach { node in
             if let name = try? node.represented().string {
                 let deps = dep.dependsTopLevel
+                    .filter { $0.tag.description != "!private" }
                     .compactMap { (try? $0.represented().string) }
                     .filter { $0 != name }
+                    .filter { systemSymbols.contains($0) == false }
+                    .filter { $0.contains("NS") == false }
                     .map { DependencyTree.Dependency(name: $0) }
 
                 result.append(DependencyTree(owner: name, dependencies: deps))
