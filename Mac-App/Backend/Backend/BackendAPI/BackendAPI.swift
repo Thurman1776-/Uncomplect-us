d657a14a5941eaa11d9e39b1d48d8cb9b6c69ad9
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-15 20:35:28 +0100

    Register new side effects

--- Mac-App/Backend/Backend/BackendAPI/BackendAPI.swift
+++ Mac-App/Backend/Backend/BackendAPI/BackendAPI.swift
@@ -7,6 +7,7 @@ public enum BackendAPI {
         middleware: [
             findProjectOutputDirsSideEffects(),
             parseSwiftDepsSideEffects(),
+            dependencyGraphSideEffects(),
         ]
     )
 
