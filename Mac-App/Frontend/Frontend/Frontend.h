34e6e561521364881ecfc33412f166670c81148c
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-20 19:46:55 +0100

    Add Frontend project to workspace

---
+++ Mac-App/Frontend/Frontend/Frontend.h
@@ -0,0 +1,19 @@
+//
+//  Frontend.h
+//  Frontend
+//
+//  Created by Daniel GARCÍA on 20.02.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frontend.
+FOUNDATION_EXPORT double FrontendVersionNumber;
+
+//! Project version string for Frontend.
+FOUNDATION_EXPORT const unsigned char FrontendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frontend/PublicHeader.h>
+
+
