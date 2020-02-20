07020e22dd08cb663c1a027f0ff45c14ef01d553
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-20 05:44:05 +0100

    Quick UI experiment to display backend data

--- Mac-App/Mac-App/AppDelegate.swift
+++ Mac-App/Mac-App/AppDelegate.swift
@@ -1,13 +1,18 @@
+import Backend
 import Cocoa
+import ReSwift
 import SwiftUI
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
+    let subscriber = TestTransformer()
 
     func applicationDidFinishLaunching(_: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView(transformer: subscriber)
+        BackendAPI.dispatch(DependencyPathsAction.findUrls(for: "Mac-App"))
+        BackendAPI.subscribe(subscriber)
 
         // Create the window and set the content view.
         window = NSWindow(
@@ -23,5 +28,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_: Notification) {
         // Insert code here to tear down your application
+    }
+}
+
+// Testing UI rendering only - Approach still needs to be defined
+
+final class TestTransformer: StoreSubscriber, ObservableObject {
+    typealias StoreSubscriberStateType = AppState
+    @Published private (set)var tree = [DependencyTree]()
+
+    func newState(state: AppState) {
+        DispatchQueue.main.async {
+            self.tree = state.dependencyGraphState.tree
+        }
     }
 }
--- Mac-App/Mac-App/ContentView.swift
+++ Mac-App/Mac-App/ContentView.swift
@@ -1,14 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject private var currentTree: TestTransformer
+
+    init(transformer: TestTransformer) {
+        self.currentTree = transformer
+    }
+
     var body: some View {
-        Text("Hello World")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 8) {
+            List {
+                ForEach(currentTree.tree, id: \.owner) { node in
+                    VStack(alignment: .leading) {
+                        Text("Owner: \(node.owner)")
+                            .bold()
+                            .foregroundColor(.gray)
+                        Spacer()
+                        Text("Dependency count: \(node.dependencies.count)")
+                            .italic()
+                            .foregroundColor(.yellow)
+                    }
+                }
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
