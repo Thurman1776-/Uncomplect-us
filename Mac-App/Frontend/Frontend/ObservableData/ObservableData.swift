07e71faea6d47325e937999d315212e1611cf536
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-02-23 12:39:05 +0100

    Introduce building blocks for data rendering

---
+++ Mac-App/Frontend/Frontend/ObservableData/ObservableData.swift
@@ -0,0 +1,38 @@
+//
+//  ObservableData.swift
+//  Frontend
+//
+//  Created by Daniel GARCÍA on 23.02.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - View Data building blocks
+
+
+/// `ViewInput` represents a template for observable objects that will emit updates to views via
+/// a `@Publisher`
+/// The publisher is part of the generic class as protocols cannot have property wrappers ☹️
+public protocol ViewInput {
+    associatedtype Data: Equatable
+
+    init(publisher: Data)
+    func render(_ newData: Data)
+}
+
+open class ObservableData<T: Equatable>: ViewInput, ObservableObject {
+    public typealias Data = T
+
+    @Published private(set) var publisher: T
+
+    required public init(publisher: T) {
+        self.publisher = publisher
+    }
+
+    /// This triggers a UI update  for any view defining this class as a `@ObservedObject`
+
+    public func render(_ newData: T) {
+        self.publisher = newData
+    }
+}
