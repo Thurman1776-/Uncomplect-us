a050a0612d49c4323143410d20e60479b6097a9f
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-03-17 14:48:57 +0100

    Further split view's layout

--- Mac-App/Frontend/Frontend/Views/DependencyItem.swift
+++ Mac-App/Frontend/Frontend/Views/DependencyItem.swift
@@ -3,15 +3,21 @@ import SwiftUI
 struct DependencyItem: View {
     let dependency: DependencyTreeView.Data.Dependency
 
-    var body: some View {
+    var body: some View { mainContent.contentShape(Rectangle()) }
+
+    private var mainContent: some View {
         VStack(alignment: .leading) {
             Text("Owner: \(dependency.owner)")
                 .bold()
+                .font(.headline)
                 .foregroundColor(.gray)
             Spacer()
-            Text("Dependency count: \(dependency.dependencies.count)")
+
+            VStack(alignment: .leading) {
+                Text("Dependency count: \(dependency.dependencies.count)")
                 .italic()
                 .foregroundColor(.yellow)
-        }.contentShape(Rectangle())
+            }
+        }
     }
 }
