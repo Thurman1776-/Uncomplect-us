110a95b06434d72ab0afe90ab627a6e449dc44f8
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-04-13 14:48:44 +0200

    Skip on same AppState

--- Mac-App/Mac-App/Transformers/ListViewTransformer.swift
+++ Mac-App/Mac-App/Transformers/ListViewTransformer.swift
@@ -14,12 +14,17 @@ import ReSwift
 final class ListViewTransformer: StoreSubscriber {
     typealias StoreSubscriberStateType = AppState
     private(set) var transformedData: ObservableData<DependencyTreeView.State> = .init(.initial)
+    private var previousState: AppState!
 
-    func newState(state: AppState) {
-        let viewData = mapAppStateToViewData(state)
+    func newState(state newState: AppState) {
+        guard previousState != newState else {
+            return
+        }
+
+        let viewData = mapAppStateToViewData(newState)
         if viewData.dependencies.isEmpty == false {
             transformedData.render(.success(viewData: viewData))
-        } else if let failure = state.dependencyGraphState.failure {
+        } else if let failure = newState.dependencyGraphState.failure {
             transformedData.render(.failure(failure))
         }
     }
