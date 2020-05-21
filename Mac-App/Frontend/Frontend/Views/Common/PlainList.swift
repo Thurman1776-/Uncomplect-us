c8fbdfb270db911461b7a190611c2c72b8811155
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-05-21 11:04:25 +0200

    Introduce PlainList

---
+++ Mac-App/Frontend/Frontend/Views/Common/PlainList.swift
@@ -0,0 +1,25 @@
+//
+//  PlainList.swift
+//  Frontend
+//
+//  Created by Daniel GARCÍA on 21.05.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlainList: View {
+    let title: [String]
+    let itemsColor: Color
+
+    public var body: some View {
+        List {
+            ForEach(title, id: \.id) { title in
+                Text(title)
+                    .font(.caption)
+                    .italic()
+                    .foregroundColor(self.itemsColor)
+            }
+        }
+    }
+}
