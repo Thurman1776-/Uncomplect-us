567f8a3ab1efddbb36487a9d5f24a28da0d1aece
Daniel GARCÍA <thurman.dev1776@gmail.com>
2020-05-29 14:58:58 +0200

    Update MARK:

--- Mac-App/Backend/Backend/BackendAPI/Middleware/DependencyGraphSideEffects/DependencyGraphSideEffectsSpec.swift
+++ Mac-App/Backend/Backend/BackendAPI/Middleware/DependencyGraphSideEffects/DependencyGraphSideEffectsSpec.swift
@@ -81,6 +81,6 @@ final class DependencyGraphSideEffectsSpec: QuickSpec {
     }
 }
 
-// MARK: - Mocks & stubs
+// MARK: - Mocks 
 
 private func mockSwiftDepsParser(_: [SwiftDeps]) -> [DependencyTree] { [] }
