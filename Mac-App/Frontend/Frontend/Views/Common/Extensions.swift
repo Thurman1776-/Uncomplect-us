d1b45b257a6153cd4515d2097c14813194baf8e7
Thurman1776- <thurman.dev1776@gmail.com>
2020-06-14 13:57:29 +0200

    Introduce common Color extensions

---
+++ Mac-App/Frontend/Frontend/Views/Common/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Frontend
+//
+//  Created by Daniel Garcia on 14.06.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let lightGray = Color(.sRGB, red: 70 / 255, green: 70 / 255, blue: 70 / 255, opacity: 1.0)
+    static let lightBlue = Color(.sRGB, red: 78 / 255, green: 176 / 255, blue: 204 / 255, opacity: 1.0)
+}
