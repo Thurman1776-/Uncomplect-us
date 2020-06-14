3ce7503a24af08ec0958c6f06517f00e90a5c02a
Thurman1776- <thurman.dev1776@gmail.com>
2020-06-14 21:11:11 +0200

    Introduce custom light green

--- Mac-App/Frontend/Frontend/Views/Common/Extensions.swift
+++ Mac-App/Frontend/Frontend/Views/Common/Extensions.swift
@@ -13,6 +13,7 @@ import SwiftUI
 extension Color {
     static let lightGray = Color(.sRGB, red: 70 / 255, green: 70 / 255, blue: 70 / 255, opacity: 1.0)
     static let lightBlue = Color(.sRGB, red: 78 / 255, green: 176 / 255, blue: 204 / 255, opacity: 1.0)
+    static let lightGreen = Color(.sRGB, red: 120 / 255, green: 194 / 255, blue: 179 / 255, opacity: 1.0)
 }
 
 // MARK: - String
