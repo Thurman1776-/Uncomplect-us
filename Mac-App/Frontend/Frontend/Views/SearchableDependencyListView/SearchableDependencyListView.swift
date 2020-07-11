38414c0f1cf0f26a92579f52c77c54effb9b0e49
Thurman1776- <thurman.dev1776@gmail.com>
2020-07-11 11:32:33 +0200

    Introduce SearchableDependencyListView

---
+++ Mac-App/Frontend/Frontend/Views/SearchableDependencyListView/SearchableDependencyListView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchableDependencyListView.swift
+//  Frontend
+//
+//  Created by Daniel Garcia on 09.07.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SearchableDependencyListView: View {
+    @ObservedObject private var dependencyTreeStatus: Observable<DependencyTree.Status>
+    
+    public init(dependencyTreeStatus: Observable<DependencyTree.Status>) {
+        self.dependencyTreeStatus = dependencyTreeStatus
+    }
+    
+    public var body: some View {
+        DependencyListView(dependencyTreeStatus: dependencyTreeStatus)
+    }
+}
