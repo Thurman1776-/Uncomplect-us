229904358aa9184042d76ea081f83aa0df5bdc10
Thurman1776- <thurman.dev1776@gmail.com>
2020-08-14 15:28:52 +0200

    Introduce navigation state

---
+++ Mac-App/Backend/Backend/AppState/NavigationState/NavigationState.swift
@@ -0,0 +1,33 @@
+//
+//  NavigationState.swift
+//  Backend
+//
+//  Created by Daniel Garcia on 14.08.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import ReSwift
+
+// MARK: - State
+
+public struct NavigationState: Equatable {
+    var currentNode: Node
+    var previousNode: Node?
+}
+
+// MARK: - Initial state
+
+extension NavigationState {
+    static let initialState = NavigationState(currentNode: .input, previousNode: nil)
+}
+
+// MARK: - Actions
+
+public enum NavigationStateAction: Action {
+    case transition(to: Node)
+}
+
+public enum Node {
+    case input
+    case mainScreen
+}
