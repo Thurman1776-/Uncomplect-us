e0558b6d21c93dbe994e25c9638b2bc4b2d00c6e
Thurman1776- <thurman.dev1776@gmail.com>
2020-09-27 17:46:52 +0200

    Introduce data type to handle menu bar actions

---
+++ Mac-App/Mac-App/macOSMenu/macOSMenu.swift
@@ -0,0 +1,16 @@
+//
+//  macOSMenu.swift
+//  Mac-App
+//
+//  Created by Daniel Garcia on 27.09.20.
+//  Copyright © 2020 Acphut Werkstatt. All rights reserved.
+//
+
+import Cocoa
+import Frontend
+
+struct macOSMenu {
+    func triggerNewSearch(_ sender: NSMenuItem) {
+        DefaultDispatcher.dispatch(FileMenuAction.newProjectSearch)
+    }
+}
