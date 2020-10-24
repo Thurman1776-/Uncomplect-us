8b11463a4b1bec40327bbe0cb0ff1ae99f7fe2ed
Thurman1776- <thurman.dev1776@gmail.com>
2020-10-24 21:53:56 +0200

    Run all actions in same queue

--- Mac-App/Backend/Backend/BackendAPI/Middleware/AssignActionsOnQueuesSideEffects/AssignActionsOnQueuesSideEffects.swift
+++ Mac-App/Backend/Backend/BackendAPI/Middleware/AssignActionsOnQueuesSideEffects/AssignActionsOnQueuesSideEffects.swift
@@ -13,15 +13,7 @@ func assignActionsOnQueuesSideEffects() -> Middleware<AppState> {
         { (next: @escaping DispatchFunction) in
             { action in
 
-                switch action {
-                case NavigationAction.transition:
-                    DispatchQueue.main.async { next(action) }
-                default:
-                    // Actions NEED to be dispatched from a serial queue. ReSwift does not support
-                    // concurrent queues. The thread where the actions are dispatched from is what causes
-                    // the state to change
-                    dispatchAsyncOnSerialBackendQueue { next(action) }
-                }
+                dispatchAsyncOnSerialBackendQueue { next(action) }
             }
         }
     }
