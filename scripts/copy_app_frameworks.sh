fffda9c8601b15db5bc20aa63f8b96459b507d9f
Thurman1776- <thurman.dev1776@gmail.com>
2020-11-27 14:54:55 +0100

    Delete unused scripts

--- scripts/copy_app_frameworks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-export SCRIPT_INPUT_FILE_0=${SRCROOT}"/Dependencies/macOS/ReSwift.framework"
-export SCRIPT_INPUT_FILE_COUNT=1
-
-/usr/local/bin/carthage copy-frameworks
